88adf3d3a42884accc35bec6a9c8743ab2498ee1
Ed <edmccormic@gmail.com>
2017-08-11 11:33:51 -0600

    Initial Commit

---
+++ Weathered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Weathered
+//
+//  Created by Ed McCormic on 8/11/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
